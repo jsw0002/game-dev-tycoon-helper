c5486d1d7f5f7a5cb9970ea65139ffeb5276d760
Jordan Willis <jsw0002@gmail.com>
2018-12-23 21:41:45 -0500

    added generate command for 404 page

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ const pkg = require('./package')
 
 module.exports = {
   router: { base: '/game-dev-tycoon-helper/' },
+  generate: {
+    routes: ['404'],
+    subFolders: false,
+  },
   mode: 'universal',
 
   /*
