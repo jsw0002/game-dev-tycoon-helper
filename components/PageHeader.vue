23cbeea4c0a2a37976bc2c41690576a98fac8779
Jordan Willis <jsw0002@gmail.com>
2018-12-23 21:57:38 -0500

    added fluid header image

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -2,7 +2,7 @@
   <div class="row justify-content-center title">
     <div class="col-6 center">
       <h1>Game Dev Tycoon Helper</h1>
-      <img src="../assets/images/gameDevTycoon.png" />
+      <img class="img-fluid" src="../assets/images/gameDevTycoon.png" />
     </div>
   </div>
 </template>
